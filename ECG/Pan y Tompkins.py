bf5d4e4bc6f998b3cc7c7ef7624d42e373e2f262
floquicanale <fcanale@itba.edu.ar>
2023-05-30 16:26:50 -0300

    pan y tom ventana

--- ECG/Pan y Tompkins.py
+++ ECG/Pan y Tompkins.py
@@ -3,108 +3,106 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 
-ard = serial.Serial('COM5',9600)
 
+class Pan_Tom_QRS():
 
-def pam_tom(data):
-    global ecg_buffer, picos
-    # FILTRO PASABANDAS
-    ecg_filtro = sp.signal.lfilter(b, a, linea)
-    filtrado.append = ecg_filtro
-    #Derivada
-    derivada = np.diff(ecg_filtro)
-    #Cuadrado de la señal
-    squared_signal = derivada ** 2
-    #Promedio movil en una ventana de 150 ms
-    averaged_signal = np.convolve(squared_signal, np.ones((m,))/m, mode='valid')
+    def pasabanda(self, x):
+        '''
+    Filtro PasaBnada
+    :param signal: señal de entrada 
+    :return: señal procesada
 
-    #cuales son los valores que superan el threshold y donde estan
-    threshold = threshold_factor * np.max(averaged_signal)
-    new_peaks = np.where(averaged_signal > threshold)[0]
-    picos.extend(new_peaks) #se guardan los nuevos en picos
+    Metodología:
+    Usamos un filtro pasabanda para atenuar el ruido. 
+    Necesitamos solo las señales con frecuencia de 5 a 15 Hz, 
+    para quedarnos con estas primero usamos un pasabajos y luego un pasaaltos en cadena.
+
+    Ecuacion recursiva del pasabajos:
+      y(nT) = 2y(nT - T) - y(nT - 2T) + x(nT) - 2x(nT - 6T) + x(nT - 12T)
+
+    Ecuacion recursiva del pasaaltos:
+      y(nT) = 32x(nT - 16T) - y(nT - T) - x(nT) + x(nT - 32T)
+    '''
+        # Initialize result
+        result = None
+
+        # Create a copy of the input signal
+        y = x.copy()
+        
+        # Apply the low pass filter using the equation given
+        for index in range(len(x)):
+            y[index] = x[index]
+
+            if (index >= 1):
+                y[index] += 2*y[index-1]
+
+            if (index >= 2):
+                y[index] -= y[index-2]
+
+            if (index >= 6):
+                y[index] -= 2*x[index-6]
+
+            if (index >= 12):
+                y[index] += x[index-12] 
+            
+        # Copy the result of the low pass filter
+        result = y.copy()
+        # Apply the high pass filter using the equation given
+        for index in range(len(x)):
+            result[index] = -1*y[index]
+
+            if (index >= 1):
+                result[index] -= result[index-1]
+
+            if (index >= 16):
+                result[index] += 32*y[index-16]
+
+            if (index >= 32):
+                result[index] += y[index-32]
+
+        # Normalize the result from the high pass filter
+        max_val = max(max(result),-min(result))
+        result = result/max_val
+
+        return result
     
-    # Eliminar los datos más antiguos del búfer
-    ecg_buffer = ecg_buffer[len(data):]
 
 
-#principal
-# Configuración en la visualización
-VentanaTiempo = 10   # Define la ventana de tiempo que si visualiza en tiempo real
-TiempoFinal = 20    # Define el tiempo total de adquisición 
-# Configuración de la figura
-plt.ion() 
-figura1 = plt.figure()
-figura1.suptitle('Gráfica en tiempo real', fontsize='16', fontweight='bold')
-plt.xlabel('Tiempo (s)', fontsize='14')
-plt.ylabel('Amplitud', fontsize='14')
-plt.axes().grid(True)
-
-#Flag para controlar el tiempo que corre el codigo 
-run=True
-
-# Lista para guardar datos Tiempo y Amplitud
-timepoints = []
-ydata = []
-filtrado = []
-
-# Configuración de la curva
-line1, = plt.plot(ydata, linestyle='-')
-line2, = plt.plot(ydata, linestyle='-')
-
-plt.xlim([0,VentanaTiempo])
-
-# Parámetros del filtro pasabanda
-fs = 9600  # Frecuencia de muestreo en Hz
-f1 = 5.0  # Frecuencia de corte inferior en Hz
-f2 = 15.0  # Frecuencia de corte superior en Hz
-order = 4  # Orden del filtro
-
-# Diseñar el filtro pasabanda
-nyquist = 0.5 * fs
-normalized_f1 = f1 / nyquist
-normalized_f2 = f2 / nyquist
-b, a = sp.signal.butter(order, [normalized_f1, normalized_f2], btype='band')
-
-# Configuración del algoritmo de Pan-Tompkins
-n = 10  # Longitud de la ventana de promedio móvil
-m = int(0.150 * fs)  # Longitud de la ventana de promedio móvil (150 ms)
-threshold_factor = 0.01  # Factor para calcular el umbral adaptativo (10% del máximo)
-
+'''
+Codigo principal
+Se usa un ciclo while para tomar datos de la señal a la cual se le aplica recursivamente Pan_Tom_QRS.
+Primero hay que tomar un segmento de la señal y hacerle el procedimiento, este mismo cachito se guarda para recursividad
+'''
 # Inicializar variables
+ard = serial.Serial('COM5',9600)
+fs = 9600  
+
 ecg_buffer = np.zeros(2000)  # Búfer para almacenar los últimos 2000 puntos de ECG
-picos=[]
+filtrada = np.zeros(2000)
+picos = []
 
+long_ventana = 10
+start_time = time.time()
+TiempoFinal = 50 # Define el tiempo total de adquisición
+run = True
 
-start_time = time()
+QRS_detector = Pan_Tom_QRS()
 
 try:
     while run:
-        # Leer una línea completa de datos hasat encontrar \n
+        # Leer una línea completa de datos hasta encontrar \n
         linea = ard.readline().decode().strip()
+        print(linea)
+        data = float(linea)
 
-        picos = pam_tom(linea)
+        ecg_buffer = np.append(ecg_buffer, data)
+        l = len(ecg_buffer)
 
-        # Se asignan los datos a las listas para graficar
-        ydata.append(linea)
-        timepoints.append(time()-start_time)
-        current_time = timepoints[-1]
-        
-        # Se actutaliza los datos en la grafica 
-        line1.set_xdata(timepoints)
-        line1.set_ydata(ydata)
-        
-        # Se actutaliza los datos en la grafica 
-        line2.set_xdata(timepoints)
-        line2.set_ydata(filtrado)
-        
-        plt.ylim([min(ydata)-5,max(ydata)+5])
-        # Se actualiza la ventan de observación de la gráfica
-        if current_time > VentanaTiempo:
-            plt.xlim([current_time-VentanaTiempo,current_time])
-            
-        # La ejecución termina cuando el tiempo de ejecución llega al límite
-        if timepoints[-1] > TiempoFinal: run=False        
-        
+        if(l>4000):
+            ecg_buffer = ecg_buffer[1999:] #elimino la ultima ventana
+            filtrada = QRS_detector.pasabanda(ecg_buffer)
+
 except KeyboardInterrupt:
     # Manejar la interrupción del teclado para detener la adquisición de datos
     ard.close()
+
